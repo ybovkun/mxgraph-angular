b1a6ef87d8917ffeb0bfc2f4271c47a1a4812f2a
Yuriy Bovkun <ybovkun@gmail.com>
2018-01-19 07:10:04 -0500

    updated references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editorComponent/editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UmlComponent
+    EditorComponent
   ],
   imports: [
     BrowserModule
